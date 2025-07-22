d55ae207ed3d8c383d449166020234bec1f086eb
Blevins <nathanc.blevins@gmail.com>
2025-07-22 17:30:36 -0500

    adding in files to hold the prediction model

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from schemas import UserInfo
+from ../dev_salary_prediction_model import salary_calculation_model
 
 app = FastAPI()
 
 @app.get('/')
 def root():
 	return {"Hello": "World"}
-	
 
 @app.post("/results")
 def calculate_salary(criteria: SearchCriteria):
-	return criteria
+	predicted_salary = salary_calculation_model(criteria)
+	return predicted_salary
---
+++ backend/model/dev_salary_prediction_model.py
@@ -0,0 +1,2 @@
+def salary_calculation_model(criteria):
+	return 1.00
